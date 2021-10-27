f556eeb4da76e70cca62b6bef6f4f14f61c01ab5
Jérémie Hornus <jeremie.hornus@black-foundry.com>
2021-10-27 15:32:20 +0200

    script

--- scripts/makeBeta.py
+++ scripts/makeBeta.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from defcon import Font
 from ufo2ft import compileOTF
 
 
 ufo = Font('../sources/Garima.ufo')
 otf = compileOTF(ufo, removeOverlaps=True)
-otf.save('../beta/Garima.otf')
+otf.save('../beta/%s_Garima.otf' % str(datetime.now()).split(' ')[0])
