7038c9fa0fcf35647e2df9fb9faa646f92ebb268
Jérémie Hornus <jeremie.hornus@black-foundry.com>
2021-10-27 15:48:20 +0200

    script

---
+++ beta/readme.txt
@@ -0,0 +1,1 @@
+beta fonts are saved in this folder
--- scripts/makeBeta.py
+++ scripts/makeBeta.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from defcon import Font
 from ufo2ft import compileOTF
 
-
 ufo = Font('sources/Garima.ufo')
 otf = compileOTF(ufo, removeOverlaps=True)
-otf.save('./beta/%s_Garima.otf' % str(datetime.now()).split(' ')[0])
+prfx = str(datetime.now()).split('.')[0].replace(":", "-").replace(" ", "_")
+otf.save('beta/%s_Garima.otf' % prfx)
