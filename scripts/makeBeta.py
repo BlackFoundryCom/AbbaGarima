e901d21237167ce98eedbbb6a1634a5c053df3ef
Jérémie Hornus <jeremie.hornus@black-foundry.com>
2021-10-27 15:36:10 +0200

    Update makeBeta.py

--- scripts/makeBeta.py
+++ scripts/makeBeta.py
@@ -3,6 +3,6 @@ from defcon import Font
 from ufo2ft import compileOTF
 
 
-ufo = Font('../sources/Garima.ufo')
+ufo = Font('sources/Garima.ufo')
 otf = compileOTF(ufo, removeOverlaps=True)
-otf.save('../beta/%s_Garima.otf' % str(datetime.now()).split(' ')[0])
+otf.save('beta/%s_Garima.otf' % str(datetime.now()).split(' ')[0])
